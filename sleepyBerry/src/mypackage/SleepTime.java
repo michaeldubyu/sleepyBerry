04b3941e4c8215cb4fa7ead6592e2f9effd1d32c
Michael Wu <michaeldubyu@gmail.com>
2012-02-21 12:47:06 -0800

    v1.1

--- sleepyBerry/src/mypackage/SleepTime.java
+++ sleepyBerry/src/mypackage/SleepTime.java
@@ -25,20 +25,17 @@ int minToSleep;
 	public String getSleepyTimes()
 	{
 		//convert given hour and min desired into string of desired times to sleep
-	
-		Calendar wakeAt = Calendar.getInstance();
-		wakeAt.set(Calendar.HOUR_OF_DAY,hourToSleep);
-		wakeAt.set(Calendar.MINUTE,minToSleep);
 		
 		String times = "You should try to be asleep by : ";
 				
 		for (int i=0;i<9;i++)
 		{
 			minToSleep = (minToSleep-30) % 60;
+			if (minToSleep < 0) hourToSleep --;
 			hourToSleep = (hourToSleep-1) % 24;			
 
 			if (minToSleep < 0) minToSleep = minToSleep + 60;
-			if (hourToSleep < 0) hourToSleep = hourToSleep + 23;
+			if (hourToSleep < 0) hourToSleep = hourToSleep + 24;
 			
 			String properMin;
 			
